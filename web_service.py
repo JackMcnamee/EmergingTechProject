e1bb16ca2a3a94a7126796aee36ff3a116688616
JackMcnamee <37696419+JackMcnamee@users.noreply.github.com>
2021-01-05 17:08:45 +0000

    User can enter a speed value and receive a power value

--- web_service.py
+++ web_service.py
@@ -1,60 +1,26 @@
 # flask for web app.
 import flask as fl
-from flask import jsonify
-# numpy for numerical work.
-import numpy as np
-from numpy import loadtxt
+from flask import request, render_template
 # tensor
 import tensorflow as tf
 from tensorflow import keras
 
-# Create a new web app.
+# create a web service
 app = fl.Flask(__name__)
 
-# https://machinelearningmastery.com/save-load-keras-deep-learning-models/
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = keras.models.model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-# load dataset
-dataset = loadtxt("Dataset.csv")
-# seperate power and speed values
-dataset_power = dataset.copy()
-dataset_speed = dataset_power.pop("speed")
-
-# Add root route.
-@app.route("/")
+# add home route
+@app.route('/', methods=["GET", "POST"])
 def home():
-  return app.send_static_file('index.html')
-
-# Add power route.
-@app.route('/api/power')
-def power():
-  data = {"value": np.random.uniform()}
-  return jsonify(data)
-
-
-# def ValuePredictor(to_predict_list):
-#   to_predict = np.array(to_predict_list).reshape(1,2)
-#   loaded_model = pickle.load(open("model.pkl", "rb"))
-#   result = loaded_model.predict(to_predict)
-#   return result[0]
-
-# @app.route('/predict', methods = ['POST'])
-# def result():
-#   if request.method == 'POST':
-#     to_predict_list = request.form.to_dict()
-#     to_predict_list=list(to_predict_list.values())
-#     to_predict_list = list(map(float, to_predict_list))
-#     result = ValuePredictor(to_predict_list)
-#     prediction = str(result)
-#   return render_template("predict.html", prediction=prediction)
-
-# if __name__ == "__main__":
-#  app.run(debug=True)
-
+  # Adapted from https://medium.com/techcrush/how-to-deploy-your-ml-model-in-jupyter-notebook-to-your-flask-app-d1c4933b29b5
+  if request.method == "POST":
+    # speed that user enters, cast to float
+    wind_speed = request.form.get("speed")
+    wind_speed = [float(wind_speed)]
+    # load model compiled in jupyter notebook
+    model = keras.models.load_model('model.h5')
+    # output prediction
+    prediction = model.predict(wind_speed)
+    # returns home page with prediction
+    return render_template('index.html', prediction=prediction[0])
+  # returns home page
+  return render_template('index.html')
